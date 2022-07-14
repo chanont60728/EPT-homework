b0a1867d2e5259e3e1d575057f281f5657fcef22
ชานนทร์  เหลืองประเสริฐ <78683623+chanont60728@users.noreply.github.com>
2022-07-14 12:16:23 +0700

    Update Card.py

--- Card.py
+++ Card.py
@@ -32,4 +32,37 @@ class Card():
 
     def __str__(self):
         return "({value} {suit})".format(value=self.value,suit=self.suit)
+    
+    def next1(self):
+        value_and_suit_list = [
+            ("3","club"),("3","diamond"),("3","heart"),("3","spade"),
+            ("4","club"),("4","diamond"),("4","heart"),("4","spade"),
+            ("5","club"),("5","diamond"),("5","heart"),("5","spade"),
+            ("6","club"),("6","diamond"),("6","heart"),("6","spade"),
+            ("7","club"),("7","diamond"),("7","heart"),("7","spade"),
+            ("8","club"),("8","diamond"),("8","heart"),("8","spade"),
+            ("9","club"),("9","diamond"),("9","heart"),("9","spade"),
+            ("10","club"),("10","diamond"),("10","heart"),("10","spade"),
+            ("J","club"),("J","diamond"),("J","heart"),("J","spade"),
+            ("Q","club"),("Q","diamond"),("Q","heart"),("Q","spade"),
+            ("K","club"),("K","diamond"),("K","heart"),("K","spade"),
+            ("A","club"),("A","diamond"),("A","heart"),("A","spade"),
+            ("2","club"),("2","diamond"),("2","heart"),("2","spade"),
+        ]
+
+        temp_tuple = (self.value,self.suit)
+        
+        if self.value == "2" and self.suit=="spade":
+            ans_index = 0
+        else:
+            ans_index = value_and_suit_list.index(temp_tuple) + 1
+        
+        ans = Card(value_and_suit_list[ans_index][0],value_and_suit_list[ans_index][1])
+        return ans
+    
+    def next2(self):
+        ans_of_2 = Card(self.value,self.suit)
+        ans_of_2 = ans_of_2.next1()
+        self.value = ans_of_2.value
+        self.suit = ans_of_2.suit
 
