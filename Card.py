a19b5464d3738f528ec0cefff67e85a92c808268
ชานนทร์  เหลืองประเสริฐ <78683623+chanont60728@users.noreply.github.com>
2022-07-14 11:20:06 +0700

    Update Card.py

--- Card.py
+++ Card.py
@@ -19,15 +19,17 @@ class Card():
         return (self.getScore() + test_case.getScore())%10
     
     def __lt__(self,c2):
-        if (self.value == "3" or self.value == "4" or self.value == "5" or self.value == "6" or self.value == "7" or self.value == "8" or self.value == "9" or self.value == "10"): 
-            if (c2.value == "3" or c2.value == "4" or c2.value == "5" or c2.value == "6" or c2.value == "7" or c2.value == "8" or c2.value == "9" or c2.value == "10"):
-                self.value = int(self.value)
-                c2.value = int(c2.value)
-                if self.value < c2.value:
-                    return self.value < c2.value
-        #if (self.value == "A" or self.value == "K" or self.value == "Q" or self.value == "J") or (c2.value == "A" or c2.value == "K" or c2.value == "Q" or c2.value == "J"):
-        #    return self.value < c2.value            
-    
+        suit_list = ["club", "diamond", "heart", "spade"]
+        value_list = ["3","4","5","6","7","8","9","10","J","Q","K","A","2"]
+        if self.value != c2.value:
+            # value compare section #
+            if value_list.index(self.value) < value_list.index(c2.value):
+                return value_list.index(self.value) < value_list.index(c2.value)
+        else:
+            # suit compare section #
+            if suit_list.index(self.suit) < suit_list.index(c2.suit):
+                return self.suit < c2.suit  
+
     def __str__(self):
         return "({value} {suit})".format(value=self.value,suit=self.suit)
 
