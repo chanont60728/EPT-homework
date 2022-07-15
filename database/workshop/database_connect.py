3ea1bccff92f7739f23f1947bc7f32cece0bdd14
chanont60728 <chanont60728@gmail.com>
2022-07-15 22:36:13 +0700

    Update database_connect.py

--- database/workshop/database_connect.py
+++ database/workshop/database_connect.py
@@ -30,10 +30,15 @@ class blank_database_and_table():
         # เพิ่มข้อมูลตารางและ column ของแต่ละตาราง ถ้าจะเพิ่มตารางหรือ column ให้มาเพิ่มตรงนี้#
         dict_of_SQL_command = {
         "client_detail" : "CREATE TABLE CLIENT_DETAIL(ClientID varchar(10) PRIMARY KEY not null,Firstname varchar(40) not null,Lastname varchar(40),E_mail varchar(40))",
-        "horpak_detail" : "CREATE TABLE HORPAK(horpak_and_room_id varchar(10) PRIMARY KEY not null,horpak_name varchar(10) not null,room_number int not null,ClientID varchar(10) not null)",
+        "horpak_detail" : "CREATE TABLE HORPAK(horpak_and_room_id varchar(10) PRIMARY KEY not null,horpak_name varchar(40) not null,room_number int not null,ClientID varchar(10) not null)",
         "reservation" : "CREATE TABLE RESERVATION(reserve_id varchar(10) PRIMARY KEY not null,horpak_and_room_id varchar(10) not null,ClientID varchar(10) not null,date_reserve date,date_check_in date not null)",
         "changing" : "CREATE TABLE CHANGING_ROOM(changing_id varchar(10) PRIMARY KEY not null,old_room_number int not null,new_room_number int not null,ClientID varchar(10) not null,date_changing date not null)",
-        "invoice" : "CREATE TABLE INVOICE(horpak_and_room_id varchar(10) PRIMARY KEY not null,year_invoice int not null,month_invoice int not null,rent DECIMAL(7,2) not null,water DECIMAL(7,2) not null,Electric DECIMAL(7,2),cable DECIMAL(7,2),internet DECIMAL(7,2),fridge DECIMAL(7,2),laundry DECIMAL(7,2),damage DECIMAL(7,2),bedding DECIMAL(7,2),clean DECIMAL(7,2),fine DECIMAL(7,2))"
+        "invoice" : "CREATE TABLE INVOICE(horpak_and_room_id varchar(10) PRIMARY KEY not null,year_invoice int not null,month_invoice int not null,rent DECIMAL(7,2) not null,water DECIMAL(7,2) not null,Electric DECIMAL(7,2),cable DECIMAL(7,2),internet DECIMAL(7,2),fridge DECIMAL(7,2),laundry DECIMAL(7,2),damage DECIMAL(7,2),bedding DECIMAL(7,2),clean DECIMAL(7,2),fine DECIMAL(7,2))",
+        "receive" : "CREATE TABLE RECEIVE(horpak_and_room_id varchar(10) PRIMARY KEY not null,date_receive date not null,payment_method varchar(10) not null)",
+        "payment_detail" : "CREATE TABLE PAYMENT_DETAIL(horpak_name varchar(40) PRIMARY KEY not null,date_payment date not null,salary DECIMAL(7,2),water DECIMAL(7,2),Electric DECIMAL(7,2),repairment DECIMAL(7,2), other DECIMAL(7,2))",
+        "housekeeper" : "CREATE TABLE HOUSEKEEPER(horpak_name varchar(40) PRIMARY KEY not null,housekeeper_name varchar(10) not null,date_start date,date_resign date)",
+        "electric_unit" : "CREATE TABLE ELECTRIC_UNIT(horpak_name varchar(40) PRIMARY KEY not null,month_use SMALLINT(12) not null,year_use int not null,unit_at_start int not null,unit_at_end int not null)",
+        "water_unit" : "CREATE TABLE WATER_UNIT(horpak_name varchar(40) PRIMARY KEY not null,month_use SMALLINT(12) not null,year_use int not null,unit_at_start int not null,unit_at_end int not null)"
         }
 
         mydb = mysql.connector.connect(
